41bcc7ea23032ee134d2f5db317209c50b9dcade
tidepodravioli <alecbanalan@gmail.com>
2025-05-05 14:44:17 -0700

    Starting on RCoordinateHelper

---
+++ source/RCoordinateHelper.cpp
@@ -0,0 +1,41 @@
+#include "../headers/RCoordinateHelper.hpp"
+
+RCoordinateHelper::RCoordinateHelper(VideoCapture &camera)
+{
+    m_camera = &camera;
+    m_currentFrame = nullptr;
+}
+
+void RCoordinateHelper::nextFrame(Mat &frame)
+{
+    if(m_camera != nullptr) m_currentFrame = &frame;
+}
+
+bool RCoordinateHelper::update()
+{
+    if(m_camera != nullptr)
+    {
+
+    }
+    else if(m_camera->isOpened())
+    {
+        Mat frame;
+        m_camera->read(frame);
+
+        if(!frame.empty())
+        {
+            
+        }
+    }
+    else return false;
+}
+
+RPointVect RCoordinateHelper::locateRobot()
+{
+    
+}
+
+float RCoordinateHelper::getRobotAngle()
+{
+
+}
