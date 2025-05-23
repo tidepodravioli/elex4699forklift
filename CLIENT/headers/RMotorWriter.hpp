430952c84cdd92ebb9ba8b904d95466e3b211f99
tidepodravioli <alec.banalan@yahoo.com>
2025-05-23 03:24:38 -0700

    Working manual mode!

--- CLIENT/headers/RMotorWriter.hpp
+++ CLIENT/headers/RMotorWriter.hpp
@@ -1,10 +1,14 @@
 #pragma once
 #include "RNetClient.hpp"
+#define FORK_POS_SIZE 6
 
 class RMotorWriter
 {
     private:
     RNetClient m_client;
+
+    const int FORK_POSITIONS[FORK_POS_SIZE] = {165, 150, 97, 82, 30, 15};
+    int fork_index = 0;
 
     public:
     RMotorWriter(RNetClient &client);
@@ -14,6 +18,16 @@ class RMotorWriter
     void drivef(int speed, float distance);
 
     void turn_r(float angle);
+
+    void forkUp();
+
+    void forkDown();
+
+    void forkWrite(int angle);
+
+    void forkSet(int index);
+
+    int forkIndex() {return fork_index;}
 
     void stop();
 };
--- CLIENT/source/RMotorWriter.cpp
+++ CLIENT/source/RMotorWriter.cpp
@@ -34,3 +34,25 @@ void RMotorWriter::stop()
     RControlEvent event = RControlEvent(ECOMMAND_SET, ETYPE_ANALOG, 1, vector<int>({0, 0}));
     m_client.sendEvent(event);
 }
+
+void RMotorWriter::forkWrite(int degrees)
+{
+    RControlEvent event = RControlEvent(ECOMMAND_SET, ETYPE_ANALOG, 2, degrees);
+    m_client.sendEvent(event);
+}
+
+void RMotorWriter::forkUp()
+{
+    if(fork_index + 1 < FORK_POS_SIZE)
+    {
+        forkWrite(FORK_POSITIONS[++fork_index]);
+    }
+}
+
+void RMotorWriter::forkDown()
+{
+    if(fork_index - 1 > -1)
+    {
+        forkWrite(FORK_POSITIONS[--fork_index]);
+    }
+}
